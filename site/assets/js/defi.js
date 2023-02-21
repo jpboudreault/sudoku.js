506ce2cb4ed9b679417777074ca0711bf363dbbf
Jean-Philippe Boudreault <leconcombre@gmail.com>
2023-02-20 23:40:33 -0500

    Added scaffoling for kids challenge

---
+++ site/assets/js/defi.js
@@ -0,0 +1,38 @@
+ (function(root){
+    var defi = root.defi = {};
+
+
+
+
+    /*
+     * En JavaScript une fonction peut retourner une valeur avec la commande 'return'
+     * Dans cet exercice retourne la valeur 1.
+     */
+    defi.retourneUn = function(){
+        // ton code ici
+    };
+
+
+
+    /*
+    * En JavaScript une chaine de caractère est appelée String et est délimitée au début et à la fin par "" ou ''
+    * Dans cet exercice retourne la valeur salut.
+    */
+    defi.returneChaine = function(){
+       // ton code ici
+    };
+
+
+
+    /*
+     * Un sudoku a 81 cases. Dans ce jeu les valeurs des cases sont contenues dans une chaine de caracteres (String).
+     * Chaque case peut avoir une valeur de 1 à 9 ou être inconnue ce qui est représenté par un '.'
+     * Retourne une chaine avec les 81 caractères.
+     */
+    defi.returneSudoku = function(){
+       // ton code ici
+    };
+
+
+})(this);
+
