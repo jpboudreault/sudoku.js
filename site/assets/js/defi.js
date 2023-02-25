edd984bb214b8f0778e291708d44a55d7c10a22b
Jean-Philippe Boudreault <leconcombre@gmail.com>
2023-02-24 23:38:19 -0500

    basic functions to validate get squares and stuff

--- site/assets/js/defi.js
+++ site/assets/js/defi.js
@@ -10,7 +10,9 @@
      */
     defi.retourneUn = function(){
         // ton code ici
+        return 1;
     };
+
 
 
 
@@ -18,9 +20,11 @@
     * En JavaScript une chaine de caractère est appelée String et est délimitée au début et à la fin par "" ou ''
     * Dans cet exercice retourne la valeur salut.
     */
-    defi.retourneChaine = function(){
+    defi.retourneUneChaine = function(){
        // ton code ici
+       return "2";
     };
+
 
 
 
@@ -29,10 +33,103 @@
      * Chaque case peut avoir une valeur de 1 à 9 ou être inconnue ce qui est représenté par un '.'
      * Retourne une chaine avec les 81 caractères.
      */
-    defi.retourneSudoku = function(){
+    defi.retourneUnSudoku = function(){
        // ton code ici
+       return "012345678901234567890123456789012345678901234567890123456789012345678901234567891"
+    };
+
+
+
+
+    /*
+     * Dans cet exercice, tu dois extraire les valeurs de la première ligne d'une chaine de sudoku.
+     * Ta fonction recoit une valeur en argument nommée 'sudoku' qui contient 81 charactères
+     *
+     */
+    defi.retournePremiereLigne = function(sudoku){
+       // ton code ici
+       var s = "";
+       for (let i = 0; i < 9; i++) {
+         s += sudoku[i];
+      }
+       return s;
+    };
+
+    /*
+     * Dans cet exercice, tu dois extraire les valeurs de la première ligne d'une chaine de sudoku.
+     * Ta fonction recoit une valeur en argument nommée 'sudoku' qui contient 81 charactères
+     *
+     */
+    defi.retourneUneLigne = function(sudoku, ligne){
+       // ton code ici
+       var s = "";
+       for (let i = 0; i < 9; i++) {
+         s += sudoku[i + 9*ligne];
+      }
+       return s;
+    };
+
+    /*
+     * Un sudoku a 81 cases. Dans ce jeu les valeurs des cases sont contenues dans une chaine de caracteres (String).
+     * Chaque case peut avoir une valeur de 1 à 9 ou être inconnue ce qui est représenté par un '.'
+     * Retourne une chaine avec les 81 caractères.
+     */
+    defi.retournePremiereColone = function(sudoku){
+       // ton code ici
+       var s = "";
+       for (let i = 0; i < 9; i++) {
+         s += sudoku[9*i];
+      }
+       return s;
+    };
+    /*
+     * Un sudoku a 81 cases. Dans ce jeu les valeurs des cases sont contenues dans une chaine de caracteres (String).
+     * Chaque case peut avoir une valeur de 1 à 9 ou être inconnue ce qui est représenté par un '.'
+     * Retourne une chaine avec les 81 caractères.
+     */
+    defi.retourneUneColone = function(sudoku, colonne){
+       // ton code ici
+       var s = "";
+       for (let i = 0; i < 9; i++) {
+         s += sudoku[9*i + colonne];
+      }
+       return s;
+    };
+
+    /*
+     * Un sudoku a 81 cases. Dans ce jeu les valeurs des cases sont contenues dans une chaine de caracteres (String).
+     * Chaque case peut avoir une valeur de 1 à 9 ou être inconnue ce qui est représenté par un '.'
+     * Retourne une chaine avec les 81 caractères.
+     */
+    defi.retournePremierBloc = function(sudoku){
+       // ton code ici
+      var s = "";
+      for (let i = 0; i < 3; i++) {
+         for (let j = 0; j < 3; j++) {
+            s += sudoku[j + 9 * i];
+        }
+      }
+      return s;
+    };
+
+    /*
+     * Un sudoku a 81 cases. Dans ce jeu les valeurs des cases sont contenues dans une chaine de caracteres (String).
+     * Chaque case peut avoir une valeur de 1 à 9 ou être inconnue ce qui est représenté par un '.'
+     * Retourne une chaine avec les 81 caractères.
+     */
+    defi.retourneUnBloc = function(sudoku, bloc){
+       // ton code ici
+      var s = "";
+      var colOffset = Math.floor(bloc / 3);
+      var ligneOffset = bloc % 3;
+      for (let i = 0; i < 3; i++) {
+         for (let j = 0; j < 3; j++) {
+            let x = j + colOffset * 3 + 9 * (i + ligneOffset * 3);
+            s += sudoku[x];
+        }
+      }
+      return s;
     };
 
 
 })(this);
-
--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,50 @@
 module('Defi');
 
+var puzz = [
+    ".........9......84.623...5....6...453...1...6...9...7....1.....4.5..2....3.8....9",
+    "174589362953261784862347951219673845387415296546928173628194537495732618731856429"
+];
+
 test("retourneUn", function(){
     ok(defi.retourneUn() == 1, defi.retourneUn() + " -> " + 1);
 });
 
-test("retourneChaine", function(){
-    ok(typeof defi.retourneChaine() === "String", typeof defi.retourneUn() + " -> String");
+test("retourneUneChaine", function(){
+    ok(typeof defi.retourneUneChaine() === "string", typeof defi.retourneUneChaine() + " -> string");
 });
 
-
-test("retourneSudoku", function(){
-    var s = defi.retourneSudoku();
-    ok(typeof s === "String", typeof s + " -> String");
+test("retourneUnSudoku", function(){
+    var s = defi.retourneUnSudoku();
+    ok(typeof s === "string", typeof s + " -> string");
     ok(s.length == 81 , s.length + " -> 81");
+});
+
+test("retournePremiereLigne", function(){
+    var s = defi.retournePremiereLigne(puzz[1]);
+    ok(s ===  "174589362", s + " -> '174589362'");
+});
+
+test("retourneUneLigne", function(){
+    var s = defi.retourneUneLigne(puzz[1],2);
+    ok(s ===  "862347951", s + " -> '862347951'");
+});
+
+test("retournePremiereColone", function(){
+    var s = defi.retournePremiereColone(puzz[1]);
+    ok(s ===  "198235647", s + " -> '198235647'");
+});
+
+test("retourneUneColone", function(){
+    var s = defi.retourneUneColone(puzz[1],1);
+    ok(s ===  "756184293", s + " -> '756184293'");
+});
+
+test("retournePremierBloc", function(){
+    var s = defi.retournePremierBloc(puzz[1]);
+    ok(s ===  "174953862", s + " -> '174953862'");
+});
+
+test("retourneUnBloc", function(){
+    var s = defi.retourneUnBloc(puzz[1],8);
+    ok(s ===  "537618429", s + " -> '537618429'");
 });
